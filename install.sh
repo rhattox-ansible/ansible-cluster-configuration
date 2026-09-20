c6d18055d33f59fd3c0a6246ecf92e20fae85705
rhattox <120387348+rhattox@users.noreply.github.com>
2026-09-19 22:36:34 -0300

    fix pattern

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# 0. Clean up
+rm -rf roles/
 
 # 1. Download/update roles
 ansible-galaxy install -r requirements.yaml -p ./roles
 
 # 2. Execute dry run
-ansible-playbook -i inventory k8s_package.yaml --check
+# ansible-playbook -i hosts k8s_package.yaml --check
+ansible-playbook -i hosts k8s_package.yaml --ask-pass -K
